ae7527fe0d59c02b555dad02fbe52e1f84a5e825
Seokwon-UAL <whitehorsediv@gmail.com>
2025-12-21 22:37:33 +0000

    그리드 적용
    cell manager 에서 이제 거리계산을 할차례임

--- Microverse02/Assets/Scripts/CellManager.cs
+++ Microverse02/Assets/Scripts/CellManager.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class CellManager : MonoBehaviour
 {
-    private List<Cell> cells = new List<Cell>();
-    // Start is called before the first frame update
+    SpatialHash spatialHash;
+
     public float cellSpeed = 2f;
+    [SerializeField] float BoxSize = 4f;
+
+    private List<Cell> cells = new List<Cell>();
     class Cell
     {
         public Vector2 currentPos;
@@ -24,16 +27,19 @@ public class CellManager : MonoBehaviour
     //velocity 는 vector 2 의 좌표를 하나 찍고 그걸  0,0 로 직선연결한다 가정. 끝부분에 화살표를 단것이라 보면 됨: 방향+힘
     void Start()
     {
+        spatialHash = new SpatialHash(BoxSize);
+
         for (int i = 0; i < 10; i++)
         {
             Cell c = new Cell();
             c.currentPos = Random.insideUnitCircle * 5f;
             c.currentVelocity = Random.insideUnitCircle.normalized;
-
             c.cellRadius = 2f;
             c.detectRadius = 4f;
             cells.Add(c);
         }
+
+        
     }
 
     // Update is called once per frame
--- Microverse02/Assets/Scripts/GridManager.cs
+++ Microverse02/Assets/Scripts/GridManager.cs
@@ -1,18 +1,70 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridManager : MonoBehaviour
+
+public class SpatialHash
 {
-    // Start is called before the first frame update
-    void Start()
+    readonly float boxSize;
+    readonly float inboxSize;
+
+    readonly Dictionary<long, List<int>> buckets = new Dictionary<long, List<int>>(1024);
+
+    public SpatialHash(float hashsize)
     {
-        
+        boxSize = Mathf.Max(0.0001f, hashsize);
+        inboxSize = 1f / boxSize;
+    
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Clear()
     {
-        
+        foreach (var key in buckets)
+        {
+            key.Value.Clear();  // 키값도 제거?
+        }
+
+    }
+
+    public void Insert(Vector2 pos, int index)
+    {
+        var c = WorldHash(pos);
+        long key = Hash(c.x, c.y);
+
+        if(!buckets.TryGetValue(key, out var list))
+        {
+            list = new List<int>(16);
+            buckets.Add(key, list);
+        }
+        list.Add(index);
+    }
+
+    public IEnumerable<int> Query(Vector2 pos)
+    {
+        var c = WorldHash(pos);
+
+        for (int dy=-1;dy<=1;dy++)
+            for (int dx=-1;dx<=1;dx++)
+            {
+                long key = Hash(c.x +dx, c.y + dy);
+
+                if (!buckets.TryGetValue(key, out var list)) continue;
+
+                for (int i = 0; i < list.Count;i++ )
+                {
+                    yield return list[i];
+                }
+            }
+    }
+
+    Vector2Int WorldHash(Vector2 pos)
+    {
+        int x = Mathf.FloorToInt(pos.x* inboxSize);
+        int y = Mathf.FloorToInt(pos.y * inboxSize);
+        return new Vector2Int(x, y);
+    }
+
+    static long Hash(int x, int y)
+    {
+        return ((long)(uint)x << 32) |(uint)y;
     }
 }
