2c9f9d0038bf5db477ad14920b9820436aff8a1e
Seokwon-UAL <hongart1108@naver.com>
2026-02-05 08:10:08 +0000

    tutorial02

--- Microverse02/Assets/Scripts/Manager/TutorialManager.cs
+++ Microverse02/Assets/Scripts/Manager/TutorialManager.cs
@@ -1,19 +1,85 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 lastMousePos;
+
+    public enum StepType
     {
-        
+        ShowMessage,
+        HoverMouse
     }
 
-    // Update is called once per frame
-    void Update()
+    [Serializable]
+    public class Step
     {
-        
+        public string message;
+        public StepType type;
+    }
+
+    [SerializeField] private TMP_Text tutorialText;
+
+    private List<Step> steps = new List<Step>();
+    private int stepIndex = -1;
+
+    private void Start()
+    {
+        // Define tutorial steps in code
+        steps.Add(new Step
+        {
+            type = StepType.HoverMouse,
+            message = "Hover the mouse to control!"
+        });
+
+        steps.Add(new Step
+        {
+            type = StepType.ShowMessage,
+            message = "Well done"
+        });
+
+        Advance();
+    }
+
+    private void Update()
+    {
+        if (stepIndex < 0 || stepIndex >= steps.Count) return;
+
+        Step s = steps[stepIndex];
+
+        if (s.type == StepType.HoverMouse)
+        {
+            Vector2 currentMouse = Input.mousePosition;
+            float moved = (currentMouse - lastMousePos).sqrMagnitude;
+
+            lastMousePos = currentMouse;
+
+            if (moved > 9f)
+            {
+                Advance();
+            }
+        }
+    }
+
+    private void Advance()
+    {
+        stepIndex++;
+
+        if (stepIndex >= steps.Count)
+        {
+            tutorialText.text = "";
+            return;
+        }
+
+        Step s = steps[stepIndex];
+
+        tutorialText.text = s.message;
+
+        if (s.type == StepType.HoverMouse)
+        {
+            lastMousePos = Input.mousePosition;
+        }
     }
 }
