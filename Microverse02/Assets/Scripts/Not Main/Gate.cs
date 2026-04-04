bc9db7e1cc8c2267431f57794d32137d663fc1a3
Seokwon-UAL <hongart1108@naver.com>
2026-04-04 23:25:28 +0100

    gate audio

--- Microverse02/Assets/Scripts/Not Main/Gate.cs
+++ Microverse02/Assets/Scripts/Not Main/Gate.cs
@@ -8,6 +8,11 @@ public class Gate : MonoBehaviour
     [SerializeField] Animator animator;
     [SerializeField] CellManager cellManager;
 
+    [Header("Audio")]
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip gateOpenSound;
+    [SerializeField] AudioClip gateBeeping;
+
     bool currentState;
 
     public void SetOpen(bool open)
@@ -18,6 +23,13 @@ public class Gate : MonoBehaviour
         if (animator!= null)
         {
             animator.SetBool("AllPressed", open);
+
+            if (audioSource != null && gateOpenSound != null && gateBeeping != null)
+            {
+                audioSource.PlayOneShot(gateOpenSound);
+                audioSource.PlayOneShot(gateBeeping);
+
+            }
         }
 
 
