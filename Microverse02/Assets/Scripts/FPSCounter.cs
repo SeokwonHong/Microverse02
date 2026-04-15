e4f049ff1a243ea27bad01de89350141f04b5fb2
Seokwon-UAL <hongart1108@naver.com>
2026-04-15 05:43:21 +0100

    mouse pointer added

--- Microverse02/Assets/Scripts/FPSCounter.cs
+++ Microverse02/Assets/Scripts/FPSCounter.cs
@@ -9,18 +9,9 @@ public class FPSCounter : MonoBehaviour
         deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
     }
 
-
     void OnGUI()
     {
         float fps = 1.0f / deltaTime;
-
-        GUIStyle style = new GUIStyle(GUI.skin.label);
-        style.alignment = TextAnchor.UpperRight;
-
-        GUI.Label(
-            new Rect(0, 10, Screen.width - 10, 30),
-            $"FPS: {Mathf.Ceil(fps)}",
-            style
-        );
+        GUI.Label(new Rect(10, 10, 150, 30), $"FPS: {Mathf.Ceil(fps)}");
     }
 }
