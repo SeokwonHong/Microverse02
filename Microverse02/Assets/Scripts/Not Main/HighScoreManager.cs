dd468aeabd7aaa510038cecd70e9ac2672f0eda7
Seokwon-UAL <hongart1108@naver.com>
2026-07-10 04:27:36 +0100

    HYDRA1.5.1

--- Microverse02/Assets/Scripts/Not Main/HighScoreManager.cs
+++ Microverse02/Assets/Scripts/Not Main/HighScoreManager.cs
@@ -7,7 +7,7 @@ public class HighScoreManager : MonoBehaviour
     const string ScoreKeyPrefix = "HighScore_";
     const string SaveVersionKey = "SaveVersion";
 
-    string currentSaveVersion = "1.5"; // change this every new build
+    string currentSaveVersion = "1.5.1"; // change this every new build
 
     void Awake()
     {
